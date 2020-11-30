200d1d93fb39fca38891185d0f02ca4cadb05719
alexandershubin <shubin8@mail.ru>
2020-11-30 23:33:29 +0300

    написал переключатель слайдов

--- source/js/script.js
+++ source/js/script.js
@@ -1,43 +1,34 @@
-// let poll = {
-//   items: document.querySelectorAll('.questionnaire'),
-//   btns: document.querySelectorAll('.btn-next'),
-//   progress: document.querySelectorAll('.progress--opacity'),
-//   count: 1,
-//   start() {
-//     this.allHidden()
-//     this.items[0].style.display = 'flex'
-//     this.progress[0].style.opacity = '1'
-//
-//     this.btns.forEach(btn => {
-//       btn.addEventListener('click', () => {
-//         this.nextItem()
-//       })
-//     })
-//   },
-//   nextItem() {
-//     this.allHidden()
-//     this.items[this.count].style.display = 'flex'
-//     this.progress[this.count].style.opacity = '1'
-//     this.count++
-//   },
-//   allHidden() {
-//     this.items.forEach(item => {
-//       item.style.display = 'none'
-//     })
-//   }
-// }
-// poll.start();
+(function () {
+  'use strict';
 
+  var selector = {
+    $button: document.querySelectorAll('.btn-next'),
+    $items: document.querySelectorAll('.questionnaire'),
+    count: 1
+  };
 
-$(document).ready(function () {
-  const items = $('.questionnaire'),
-        btns = $('.btn-next');
-  let num = 1;
-  $('.questionnaire:eq(0)').show();
+  function start() {
+    allHidden()
+    selector.$items[0].show();
 
+    selector.$button.forEach(item => {
+      item.addEventListener('click', function (event) {
+        nextItem()
+      });
+    })
+  }
 
-    // $('.btn-next').click(function () {
-    //   $('.questionnaire:eq('+num)')')
-    //   num++
-    // })
-});
+  function nextItem() {
+    allHidden()
+    selector.$items[selector.count].show()
+    selector.count++
+  }
+
+  function allHidden() {
+    selector.$items.forEach(item => {
+      item.slideDown()
+    })
+  }
+
+  start()
+}());
